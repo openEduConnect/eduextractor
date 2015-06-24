46c3ded3d115db48cb69233f2eb92e9d39373447
hunterowens <owens.hunter@gmail.com>
2015-06-24 14:37:55 -0500

    toSQL function. #5

--- eduextractor/base.py
+++ eduextractor/base.py
@@ -1,5 +1,6 @@
-from eduextractor import secrets
-
+from eduextractor.config import secrets
+import pandas as pd
+from sqlalchemy import create_engine
 
 class RemoteSource():
     """A remote source of data, 
@@ -18,8 +19,27 @@ class MAPSource(TestSource):
     """
     def __init(self):
         pass
+
+def _create_connection_string():
+    dialect = secrets['sql']['dialect'] 
+    username = secrets['sql']['username']  
+    password = secrets['sql']['password']
+    host = secrets['sql']['host'] 
+    port = secrets['sql']['port']
+    dbname = secrets['sql']['dbname']
+    conn_str = dialect + "://" + username + ":" + password + "@" + host + ":" + str(port) + "/" + dbname
+    return conn_str
+
+def _make_connection_to_sql_db():
+    return create_engine(_create_connection_string())
+
 def toSQL(file,table):
     """Converts a CSV file into a SQL table. 
     Should be a flag or something.
+
+    Arguments:
+    file: path to CSV to load into a table
+    table: SQL table to load into 
     """
-    pass
+    df = pd.read_csv(file)
+    df.to_sql(table,conn=_make_connection_to_sql_db())
