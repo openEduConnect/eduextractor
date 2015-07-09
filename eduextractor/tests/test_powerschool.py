b3e49e2ffe28d35f6d8c4f09045e0d727e31963b
hunterowens <owens.hunter@gmail.com>
2015-07-09 16:10:28 -0400

    fixing tests

--- eduextractor/tests/test_powerschool.py
+++ eduextractor/tests/test_powerschool.py
@@ -12,13 +12,17 @@ class test_powerschool_frontend(unittest.TestCase):
         """
         Test if we can login
         """
-        self.ps.login() 
-        assert self.assertEqual(self.ps.dr.title,u'Start Page')
+        self.ps.login()
+        self.assertEqual(self.ps.dr.title, u'Start Page')
 
 
 class test_powerschool_admin(unittest.TestCase):
+    
+    @classmethod
+    def setUp(self):
+        self.ps = PowerSchoolAdmin()
 
-    def setUp():
-        ps = PowerSchoolAdmin()
-
-
+    def testLogin(self):
+        self.ps.login()
+        self.assertEqual(self.ps.dr.title, 
+                         u'Powerschool System Management')
