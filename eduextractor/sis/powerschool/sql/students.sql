599d61c4dc94aa406c126d51cc288c4278aa46b8
almartin82 <almartin@gmail.com>
2015-07-09 14:22:21 -0400

    basic student (person) sql for powerschool

---
+++ eduextractor/sis/powerschool/sql/students.sql
@@ -0,0 +1,9 @@
+SELECT s.id, 
+       s.first_name,
+       s.middle_name,
+       s.last_name,
+       s.gender,
+       s.dob
+FROM students s
+--0 indicates currently enrolled students in PowerSchool
+WHERE s.enroll_status = 0
