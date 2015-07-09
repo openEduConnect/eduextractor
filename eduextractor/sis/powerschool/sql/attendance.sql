045a564d792bec5b13be236c3e099a05a0cb5cc3
almartin82 <almartin@gmail.com>
2015-07-09 15:50:52 -0400

    attendance query.  closes #13

---
+++ eduextractor/sis/powerschool/sql/attendance.sql
@@ -0,0 +1,9 @@
+SELECT a.studentid,
+       a.schoolid,
+	   a.att_date,
+	   a.att_code,
+	   a.presence_status_cd
+FROM ps_attendance_daily a
+JOIN students s
+  ON a.studentid = s.id
+ AND s.enroll_status = 0
