8e6ff8ab825f63bc9470485c6c17492cabe478b8
hunterowens <owens.hunter@gmail.com>
2015-07-15 11:34:46 -0500

    fucking around with templates

--- eduextractor/sis/powerschool/sql/students.sql
+++ eduextractor/sis/powerschool/sql/students.sql
@@ -6,4 +6,17 @@ SELECT s.id,
        s.dob
 FROM students s
 --0 indicates currently enrolled students in PowerSchool
-WHERE s.enroll_status = 0
+WHERE s.enroll_status = 0;alternatecolor]
+
+<tr class="oddrow" >
+	<td><font size="1.5">          ~(id;1)           </font>           </td>
+	<td><font size="1.5">          ~(first_name;t)    </font>           </td>
+	<td><font size="1.5">          ~(middle_name;t)    </font>           </td>
+	<td><font size="1.5">          ~(last_name;t)    </font>           </td>
+	<td><font size="1.5">          ~(gender;t)    </font>           </td>
+	<td><font size="1.5">          ~(dob;t)    </font>           </td>
+</tr>
+[/tlist_sql]
+
+
+
