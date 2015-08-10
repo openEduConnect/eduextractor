d79a9dc7f07b3f60e9605356f6814428306dfad5
hunterowens <owens.hunter@gmail.com>
2015-08-10 15:23:08 -0500

    more map stuff

--- eduextractor/map_loader.py
+++ eduextractor/map_loader.py
@@ -7,27 +7,28 @@ from eduextractor.config import secrets
 NWEA_USERNAME = secrets['nwea_map']['username'] 
 NWEA_PASSWORD = secrets['nwea_map']['secret']
 
+
 def load_map_files():
-## send a GET request to the API endpoint
+    # send a GET request to the API endpoint
     print 'Connecting to NWEA...'
-    cdf_url = 'https://api.mapnwea.org/services/reporting/dex'
-    r = requests.get(cdf_url, auth=(NWEA_USERNAME,NWEA_PASSWORD))
+    cdf_url = "https://api.mapnwea.org/services/reporting/dex"
+    r = requests.get(cdf_url, auth=(NWEA_USERNAME, NWEA_PASSWORD))
 
     print r.reason
-## if the request is successful...
+# if the request is successful...
     if r.status_code == 200:
         print 'Downloading CDF...'
-        ## convert the response content into a zipfile
+        # convert the response content into a zipfile
         z = ZipFile(StringIO(r.content))
         zfiles = z.namelist()
 
         for i, f in enumerate(zfiles):    
             print 'Extracting', f
-            ## open/extract file from zip
+            # open/extract file from zip
             cdf = z.open(f)
-            ## read into dataframe
+
+            # read into dataframe
             df = pd.read_csv(cdf)        
-            ## define the table name        
             df.to_csv('/tmp/map.csv')
     print 'Done!'
 
