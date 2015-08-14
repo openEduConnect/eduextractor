a438ae0ee66850eb142168ed94bb44c81671b2a8
hunterowens <owens.hunter@gmail.com>
2015-08-14 11:04:36 -0500

    table stub

--- eduextractor/table.py
+++ eduextractor/table.py
@@ -1,5 +1,17 @@
 class Table(): 
-    """A representation a table to be subclassed
-    by other files? 
+    """A representation a table. 
+    has source and transformation methods. 
+
+    Attributes
+    name (str)
+    html_source (netloc)
+    headers (list of str)
+
     """
-    pass
+    def __init__(self, name, html_source, headers):
+        self.name = name
+        self.html_source = html_source
+        self.headers = headers
+    
+
+
