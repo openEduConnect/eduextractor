e1add8fd41583641c8e8898ddd41a1d86dba9753
hunterowens <owens.hunter@gmail.com>
2016-01-11 15:36:52 -0600

    removing all the html nonsense

--- eduextractor/sis/powerschool/sql/adhoc_studentemail.sql
+++ eduextractor/sis/powerschool/sql/adhoc_studentemail.sql
@@ -2,12 +2,3 @@ SELECT ID AS personid,
        'emailaddress' AS key,
        ps_customfields.getStudentscf(id, 'StudentEmail') AS value
 FROM students s
-;alternatecolor]
-
-<tr class="oddrow" >
-	<td><font size="1.5">          ~(id;1)           </font>           </td>
-	<td><font size="1.5">          ~(personid;t)    </font>           </td>
-	<td><font size="1.5">          ~(value;t)    </font>           </td>
-</tr>
-[/tlist_sql]
-
--- eduextractor/sis/powerschool/sql/attendance.sql
+++ eduextractor/sis/powerschool/sql/attendance.sql
@@ -5,14 +5,4 @@ SELECT a.studentid,
 	   a.presence_status_cd
 FROM ps_attendance_daily a
 JOIN students s
-  ON a.studentid = s.id;alternatecolor]
-
-<tr class="oddrow" >
-	<td><font size="1.5">          ~(id;1)           </font>           </td>
-	<td><font size="1.5">          ~(schoolid;t)    </font>           </td>
-	<td><font size="1.5">          ~(att_date;t)    </font>           </td>
-	<td><font size="1.5">          ~(att_code;t)    </font>           </td>
-	<td><font size="1.5">          ~(presence_status_cd;t)    </font>           </td>
-</tr>
-[/tlist_sql]
-
+  ON a.studentid = s.id
