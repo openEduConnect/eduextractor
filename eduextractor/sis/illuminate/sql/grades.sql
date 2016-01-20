84df18302079498ce1bff1884dc86c91edc7e03d
nicolejeong <njeong@kippla.org>
2016-01-19 16:29:17 -0800

    Update grades.sql

--- eduextractor/sis/illuminate/sql/grades.sql
+++ eduextractor/sis/illuminate/sql/grades.sql
@@ -4,13 +4,14 @@ SELECT distinct stu.local_student_id as studentid
 	,academic.grade as grade 
 	,NULL as percent 
 	,academic.count_towards_gpa
-	,gpa_points as gpa_addedvalue 
+	,academic.gpa_points as gpa_addedvalue 
 	,sgpa.is_published
+	,gp.grading_period_end_date
 FROM public.student_grades sg
 JOIN public.students stu on sg.student_id = stu.student_id 
 JOIN public.grades academic ON academic.grade_id = sg.grade_id
 JOIN public.section_grading_period_aff sgpa on sg.sgpa_id = sgpa.sgpa_id
-JOIN public.grading_periods USING (grading_period_id)
+JOIN public.grading_periods gp USING (grading_period_id)
 JOIN public.term_info ti USING (term_id)
 JOIN public.section_course_aff secc on sgpa.section_id = secc.section_id
 JOIN public.courses cou on secc.course_id = cou.course_id 
