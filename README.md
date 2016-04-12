fea15fdfd5f87bd5db90b5c322a7349cff17b6c1
almartin82 <almartin@gmail.com>
2016-04-12 14:35:51 -0400

    python 2 and 3 compatibility for map_loader. closes #36

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Extraction-Transformation Tools for Education Data.
 
 [Docs](http://eduextractor.readthedocs.org/en/latest/)
 
-## Installing cx_Oracle
-[Because you have to](https://gist.github.com/thom-nic/6011715)
+## Oracle drivers for Powerschool
+If using the Powerschool features of eduextractor, you'll need Oracle drivers.
+[Installing cx_Oracle](https://gist.github.com/thom-nic/6011715)
 
 ## Supported Datasources
 * NWEA MAP Scores
@@ -20,7 +21,7 @@ Extraction-Transformation Tools for Education Data.
 You should create a file called .eduextractor.yml in your home directory.
 
 You may change the location that eduextractor looks for the config file by
-creating an envirnment variable called EDUEXTRACTOR\_CONFIG and setting it to
+creating an environment variable called EDUEXTRACTOR\_CONFIG and setting it to
 be the path to your config file as follows:
 
     $ export EDUEXTRACTOR_CONFIG=/path/to/my_eduextractor_config.yml
@@ -28,5 +29,8 @@ be the path to your config file as follows:
 The contents of the .eduextractor.yml file should look like the following,
 but with all of the blanks filled in:
 
-    - ### PLACEHOLDER ###
-    - ### PLACEHOLDER ###
+```
+nwea_map:
+  username: you@somedistrict.org
+  secret: yourlongpasswordcopypastedfromlastpass
+```
--- eduextractor/map_loader.py
+++ eduextractor/map_loader.py
@@ -1,33 +1,38 @@
+from __future__ import print_function
 import requests
 import pandas as pd
 from zipfile import ZipFile
-from StringIO import StringIO
+from io import StringIO
 from eduextractor.config import secrets
 
 NWEA_USERNAME = secrets['nwea_map']['username']
 NWEA_PASSWORD = secrets['nwea_map']['secret']
 
-
 def load_map_files():
+    """
+    Hits the NWEA automated reporting API and downloads the comprehensive data file.
+    Returns: writes 'map.csv' to /tmp
 
+    """
     # send a GET request to the API endpoint
-    print 'Connecting to NWEA...'
+    print('Connecting to NWEA...')
     cdf_url = "https://api.mapnwea.org/services/reporting/dex"
     r = requests.get(cdf_url, auth=(NWEA_USERNAME, NWEA_PASSWORD))
 
-    print r.reason
+    print(r.reason)
 
     # if the request is successful..
     df_list = []
+    print(r.status_code)
     if r.status_code == 200:
-        print 'Downloading CDF...'
+        print('Downloading CDF...')
 
         # convert the response content into a zipfile
         z = ZipFile(StringIO(r.content))
         zfiles = z.namelist()
 
         for i, f in enumerate(zfiles):
-            print 'Extracting', f
+            print('Extracting', f)
 
             # open/extract file from zip
             cdf = z.open(f)
